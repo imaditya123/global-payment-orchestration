0513474324481ff4b4d93fe4f4efede3cc24fc93
imaditya123 <imadityarathore@gmail.com>
2025-11-30 03:31:41 +0530

    audit-service bug resolve

--- audit-service/src/test/java/com/global/audit/AuditEventListenerTests.java
+++ audit-service/src/test/java/com/global/audit/AuditEventListenerTests.java
@@ -3,8 +3,11 @@ package com.global.audit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @SpringBootTest
+@ActiveProfiles("test")
 public class AuditEventListenerTests {
     @Test
     void contextLoads() {
